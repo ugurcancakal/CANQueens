09f7d15315cff34ad9d85ec6f88520ebafc6a86b
@ugurc <ugurcan.cakal@gmail.com>
2020-05-16 22:11:22 +0300

    CA class created empty

---
+++ src/CANQueens/CANQueens/CA.cu
@@ -0,0 +1,20 @@
+/* Cell Assembly Class Source File
+ *
+ * 200516
+ * author = @ugurc
+ * ugurcan.cakal@gmail.com
+ */
+
+#include "CA.cuh"
+
+CA::CA() {
+    std::cout << "CA constructed" << std::endl;
+}
+
+CA::~CA() {
+    std::cout << "CA destructed" << std::endl;
+}
+
+std::string CA::toString() {
+    return "CA";
+}
---
+++ src/CANQueens/CANQueens/CA.cuh
@@ -0,0 +1,28 @@
+/* Cell Assembly Class Header File
+ *
+ * 200516
+ * author = @ugurc
+ * ugurcan.cakal@gmail.com
+ */
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif 
+
+#ifndef CA_H
+#define CA_H
+
+#include <string>
+#include <iostream>
+
+class CA {
+public:
+    CA();
+    ~CA();
+    std::string toString();
+    //CUDA_CALLABLE_MEMBER std::string toString();
+};
+
+#endif // CA_H
